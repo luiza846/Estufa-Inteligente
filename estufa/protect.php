8c1f80ff16d4910c7ac53153ba7636f6d29d02eb
Ana Luiza <112641732+luiza846@users.noreply.github.com>
2023-10-13 17:54:11 -0300

    add css para a tela protect

--- estufa/protect.php
+++ estufa/protect.php
@@ -1,11 +1,26 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/protect.css">
+    <title>PROTECT</title>
+</head>
+<body>
+    <div class="protect">
+        <?php
+            if(!isset($_SESSION)) {
+                session_start();
+            }
 
-if(!isset($_SESSION)) {
-    session_start();
-}
+            if(!isset($_SESSION['id_usuario'])) {
 
-if(!isset($_SESSION['id_usuario'])) {
+                echo "<img src='images/acesso.png' width='250'>","<br>";
+                die("<h1>Você não pode acessar esta página porque não está logado!</h1><p><br><a href=\"index.php\">ENTRAR</a></p>");
+            }
+        ?>
 
-    die("Você não pode acessar esta página porque não está logado.<p><a href=\"index.php\">Entrar</a></p>");
-}
+    </div>
+</body>
+</html>
 
