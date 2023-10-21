a45ae4124831df1a4f65f832aeb4c71932efbae7
Ana Luiza <112641732+luiza846@users.noreply.github.com>
2023-10-21 12:09:15 -0300

    Cadastrar foto da planta

--- estufa/cadasPlanta.php
+++ estufa/cadasPlanta.php
@@ -1,6 +1,6 @@
 <?php
 
-include('protect.php');
+#include('protect.php');
 
 ?>
 <!DOCTYPE html>
@@ -18,8 +18,12 @@ include('protect.php');
 </div>
 
 <div class="div1" id="main-container">
-        <form id="register-form" method="POST" action="processCadasPlanta.php">
+        <form id="register-form" method="POST" action="processCadasPlanta.php" enctype="multipart/form-data">
             <h1>CADASTRAR PLANTA</h1>
+            <!-- CARREGAR IMAGEM -->
+            <label for="foto_planta">Foto: </label>
+            <input type="file" name="foto_planta" id="foto_planta" required><br><br>
+
             <!--ocupa metade do formulario (half-box)-->
             <div class="half-box spacing">
                 Nome: <input type="text" name="campoNome" id="name" placeholder="Digite o apelido da planta">
--- estufa/processCadasPlanta.php
+++ estufa/processCadasPlanta.php
@@ -3,54 +3,63 @@
 
     #verificar se a variável de sessão chamada 'id_usuario' está definida
     if (isset($_SESSION['id_usuario'])) {
-
-            #fazer conexao com BD
-        try
-        {
-            echo "Dados gravados com sucesso!<br><br>";
-
-            #atributos
-
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            # receber dados do formulário
             $id="";
             $id_usuario = $_SESSION['id_usuario'];
             $nome=$_POST['campoNome'];
             $dat=$_POST['campoData'];
             $umidade=$_POST['campoUmidade'];
             $temperatura=$_POST['campoTemperatura'];
-
-            #imprimir os dados inseridos
-            echo "Nome: {$nome}<br>";
-            echo "Data que foi plantado: {$dat}<br>";
-            echo "Umidade: {$umidade}%<br>";
-            echo "Temperatura: {$temperatura}°C<br>";
-
-            #conexao com mysql
-            $conectaBD=new PDO("mysql:host=127.0.0.1;port=3306;dbname=estufa","root","");
-            $conectaBD->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-
-            #conexao com banco de dados operacoes_cartao
-            $dados="INSERT INTO planta(id_planta,id_usuario,nome,data_criacao,umidade,temperatura) VALUE('".$id."','".$id_usuario."','".$nome."','".$dat."','".$umidade."','".$temperatura."')";
-
-            #metodo para executar o sql
-            $conectaBD->exec($dados);
-
-            #gerando arquivo txt
-            #$historico = "Nome: ".$_POST['campoNome']."\nData que foi plantado: ".$_POST['campoData']."\nUmidade: ".$_POST['campoUmidade']."\nTemperatura: ".$_POST['campoTemperatura'];
-            #file_put_contents('HistoPlantas.txt', $historico . "\n", FILE_APPEND);
-            #FILE_APPEND permite que você adicione dados a um arquivo existente sem sobrescrever seu conteúdo anterior.
+            $arquivo = $_FILES['foto_planta'];
+        
+            # fazer conexão com o BD
+            try {
+                # Criação da conexão PDO
+                $conn = new PDO("mysql:host=localhost;dbname=estufa", "root", "");
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+                # Query do BD
+                $query_usuario = "INSERT INTO planta (id_usuario, nome, data_criacao, umidade, temperatura, imagem) VALUES (:id_usuario, :nome, :data_criacao, :umidade, :temperatura, :imagem)";
+        
+                $cad_usuario = $conn->prepare($query_usuario);
+                $cad_usuario->bindParam(':id_usuario', $id_usuario, PDO::PARAM_STR);
+                $cad_usuario->bindParam(':nome', $nome, PDO::PARAM_STR);
+                $cad_usuario->bindParam(':data_criacao', $dat, PDO::PARAM_STR);
+                $cad_usuario->bindParam(':umidade', $umidade, PDO::PARAM_STR);
+                $cad_usuario->bindParam(':temperatura', $temperatura, PDO::PARAM_STR);
+                $cad_usuario->bindParam(':imagem', $arquivo['name'], PDO::PARAM_STR);
+        
+                if ($cad_usuario->execute()) {
+                    # verificar se o usuário está enviando a foto
+                    if ((isset($arquivo['name'])) && !empty($arquivo['name'])) {
+                        # recuperar o último ID inserido no BD
+                        $ultimo_id = $conn->lastInsertId();
+        
+                        # diretório onde o arquivo será salvo
+                        $diretorio = "planta/$ultimo_id/";
+        
+                        # criar o diretório
+                        mkdir($diretorio, 0755);
+        
+                        # fazer o upload do arquivo
+                        $nome_arquivo = $arquivo['name'];
+                        move_uploaded_file($arquivo['tmp_name'], $diretorio . $nome_arquivo);
+        
+                        echo "Foto salva";
+                    } else {
+                        echo "Cadastro realizado com sucesso.";
+                    }
+                } else {
+                    echo "Erro ao cadastrar o usuário.";
+                }
+            } catch (PDOException $erro) {
+                # informar que houve erro ao fazer a conexão com o BD
+                echo "Erro na conexão com o banco de dados: " . $erro->getMessage();
+            }
         }
-        catch(PDOException $erro)
-        {
-            #informar que houve erro ao fazer a conexao com BD
-            echo "Houve erro ao fazer a conexao com banco de dados!";
-            echo "<br><br>Error: " . $erro->getMessage();
+        } else {
+            echo "Erro!";
         }
-    }
-    else {
-        echo "ID de usuário não definido na sessão.";
-    }
-?>
-<?php
-
-
-?>
+        ?>
+        
