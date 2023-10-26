7a6bdd0501aa743584799b1d7c572056d480c7ee
Ana Luiza <112641732+luiza846@users.noreply.github.com>
2023-10-25 23:08:28 -0300

    delete

--- estufa/registro.php
+++ estufa/registro.php
@@ -14,39 +14,6 @@ include('protect.php');
 <body>
     <!--DPS APAGAR-->
     <?php echo "ID_USUARIO: ", $_SESSION['id_usuario']; ?>
-    <h1>Registro</h1>
 
-    <table class="tb-registro">
-        <tr>
-            <th>ID</th>
-            <th>NOME DA AÇÃO</th>
-            <th>DATA</th>
-            <th>HORA</th>
-        </tr>
-        <tr>
-            <td>1</td>
-            <td>Irrigação</td>
-            <td>23/04/2023</td>
-            <td>05:00</td>
-        </tr>
-        <tr>
-            <td>2</td>
-            <td>Refrigeração</td>
-            <td>23/04/2023</td>
-            <td>06:00</td>
-        </tr>
-        <tr>
-            <td>3</td>
-            <td>Luz UV</td>
-            <td>23/04/2023</td>
-            <td>07:00</td>
-        </tr>
-        <tr>
-            <td>4</td>
-            <td>Irrigação</td>
-            <td>23/04/2023</td>
-            <td>16:40</td>
-        </tr>
-    </table>
 </body>
 </html>
