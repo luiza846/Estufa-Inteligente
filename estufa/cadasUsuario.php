177fb011740afbe754499023624421e82e0f672f
Ana Luiza <112641732+luiza846@users.noreply.github.com>
2023-11-11 19:30:37 -0300

    avisos cadasUsuario
    terminar de arrumar a interface na tela cadasUsuario.php
    
    Co-Authored-By: ClarinhaFlores <116900571+ClarinhaFlores@users.noreply.github.com>

--- estufa/cadasUsuario.php
+++ estufa/cadasUsuario.php
@@ -14,8 +14,77 @@
 <div class="div-cadas-usuario">
     <h1>CRIAR CONTA</h1>
 
-    <form class="form-cadas-usuario" method="POST" action="processCadasUsua.php" enctype="multipart/form-data">
+    <form class="form-cadas-usuario" method="POST" action="" enctype="multipart/form-data">
         <!-- CARREGAR IMAGEM -->
+        <div class = "div-cadas-usuario-aviso">
+        <?php
+include_once "conexao.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $id_usuario = "";
+    $nome = $_POST['campoNome'];
+    $senha = $_POST['campoSenha'];
+    $confirmSenha = $_POST['campoConfirmSenha'];
+    $arquivo = $_FILES['foto_usuario'];
+    $nSerie = $_POST['campoSerie'];
+    $email = $_POST['campoEmail'];
+
+    try {
+        if($senha == $confirmSenha){
+        // Criar uma conexão PDO
+        $conn = new PDO("mysql:host=localhost;dbname=estufa", "root", "");
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // verificar se o email e n serie existem
+        $checkQuery = $conn->prepare("SELECT n_serie, email_produto FROM produto WHERE n_serie = :nSerie AND email_produto = :email");
+        $checkQuery->bindParam(':nSerie', $nSerie, PDO::PARAM_STR);
+        $checkQuery->bindParam(':email', $email, PDO::PARAM_STR);
+        $checkQuery->execute();
+        $result = $checkQuery->fetch(PDO::FETCH_ASSOC);
+
+        if ($result) {
+            // se o email e senha estao corretos inserir dados
+            $query_usuario = "INSERT INTO usuario (nome, n_serie, email, senha, imagem) VALUES (:nome, :n_serie, :email, :senha, :imagem)";
+            $cad_usuario = $conn->prepare($query_usuario);
+            $cad_usuario->bindParam(':nome', $nome, PDO::PARAM_STR);
+            $cad_usuario->bindParam(':n_serie', $nSerie, PDO::PARAM_STR);
+            $cad_usuario->bindParam(':email', $email, PDO::PARAM_STR);
+            $cad_usuario->bindParam(':senha', $senha, PDO::PARAM_STR);
+            $cad_usuario->bindParam(':imagem', $arquivo['name'], PDO::PARAM_STR);
+
+            if ($cad_usuario->execute()) {
+                // deu certo
+                if ((isset($arquivo['name'])) && !empty($arquivo['name'])) {
+                    $ultimo_id = $conn->lastInsertId();
+                    $diretorio = "usuario/$ultimo_id/";
+                    mkdir($diretorio, 0755);
+                    $nome_arquivo = $arquivo['name'];
+                    move_uploaded_file($arquivo['tmp_name'], $diretorio . $nome_arquivo);
+                    echo "<dialog id='msgSucesso' open>
+                                <center><img src=fundoLogin/sucesso.png></center>
+                                <p>Cadastro realizado com sucesso!</p>
+                                <a href=index.php><input type=button value=VOLTAR name=btnVoltar></a>
+                            </dialog>";
+                } else {
+                    echo "Cadastro realizado com sucesso.";
+                }
+            } else {
+                echo "Erro ao cadastrar o usuário.";
+            }
+        } else {
+            echo "*Erro: Email e Número de Série incorretos.";
+        }}else{
+            echo "*Erro: Senhas diferentes!";
+        }
+    } catch (PDOException $erro) {
+        echo "Erro na conexão com o banco de dados: " . $erro->getMessage();
+    }
+} else {
+    echo "";
+}
+?>
+        </div>
         <label for="foto_usuario">Foto: </label>
         <input type="file" name="foto_usuario" id="foto_usuario" required><br><br>
         
