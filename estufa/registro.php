dd216d064b04c1a5696fcfb7e960145bb0d31a51
Ana Luiza <112641732+luiza846@users.noreply.github.com>
2023-12-02 17:01:27 -0300

    update telaRegistro

--- estufa/registro.php
+++ estufa/registro.php
@@ -2,60 +2,87 @@
 
 include('protect.php');
 
+//quantidade de linhas
+$linhasPorPagina = 15;
+
+$arquivo = 'dados.txt';
+
+// ler linhas (array)
+$linhas = file($arquivo);
+
+// contar n total de linhas
+$totalLinhas = count($linhas);
+
+// contar n total de paginas
+$totalPaginas = ceil($totalLinhas / $linhasPorPagina);
+
+// Página atual (obtida a partir do parâmetro de URL)
+$paginaAtual = isset($_GET['pagina']) ? $_GET['pagina'] : 1;
+
+// Índice inicial da linha na página atual
+$indiceInicial = ($paginaAtual - 1) * $linhasPorPagina;
+
+// Índice final da linha na página atual
+$indiceFinal = $indiceInicial + $linhasPorPagina;
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <title>Registro</title>
 </head>
-<body class = "body-registro" style="background-image: url(fundoLogin/registro.png);">
 
-<div class = "div-registro">
-    <table>
+<body class="body-registro" style="background-image: url(fundoLogin/registro.png);">
 
-        <tr>
-        <th>Data</th>
-        <th>Hora</th>
-        <th>Temperatura</th>
-        <th>Umidade</th>
-        </tr>
+    <div class="div-registro">
+        <table>
 
-        <?php 
+            <tr>
+                <th>Data</th>
+                <th>Hora</th>
+                <th>Temperatura</th>
+                <th>Umidade</th>
+            </tr>
 
-            // Caminho do arquivo TXT
-            $arquivo = 'dados.txt';
+            <?php
+            // Itera sobre as linhas da página atual
+            for ($i = $indiceInicial; $i < $indiceFinal && $i < $totalLinhas; $i++) {
 
-            // Lê todas as linhas do arquivo em um array
-            $linhas = file($arquivo);
+                $dados = explode(' ', $linhas[$i]);
 
-
-            // Itera sobre as últimas linhas
-            foreach ($linhas as $linha) {
-                // Divide a linha em partes usando espaço como delimitador
-                $dados = explode(' ', $linha);
-
-                // Atribui os valores a variáveis
                 $data = $dados[0];
                 $hora = $dados[1];
                 $temperatura = $dados[2];
                 $umidade = $dados[3];
-            
-
 
                 echo '<tr>';
-                echo '<td>'. $data .'</td>';
-                echo '<td>'. $hora .'</td>';
-                echo '<td>'. $temperatura .'</td>';
-                echo '<td>'. $umidade .'</td>';
+                echo '<td>' . $data . '</td>';
+                echo '<td>' . $hora . '</td>';
+                echo '<td>' . $temperatura . '</td>';
+                echo '<td>' . $umidade . '</td>';
                 echo '</tr>';
-            
             }
-        ?>
-    </table>
-
+            ?>
+        </table>
     </div>
+            <!-- links pag anterior e proxima -->
+            <div class="div-paginas">
+            <?php if ($paginaAtual > 1) : ?>
+                <img class = "img-ante" src="fundoLogin/anterior.png" alt="Ícone de anterior">
+                <a href="?pagina=<?php echo $paginaAtual - 1; ?>">Anterior</a>
+            <?php endif; ?>
+
+            <?php if ($paginaAtual < $totalPaginas) : ?>
+                <a class="a-posterior" href="?pagina=<?php echo $paginaAtual + 1; ?>">Próxima</a>
+                <img class = "img-prox" src="fundoLogin/proximo.png" alt="Ícone de proximo">
+            <?php endif; ?>
+        </div>
+
 </body>
+
 </html>
