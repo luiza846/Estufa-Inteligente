816f3de7349ee412a40e47249e11bdc87d6227c6
Ana Luiza <112641732+luiza846@users.noreply.github.com>
2024-05-02 10:28:18 -0300

    update index.html (ecommerce)

--- estufa/protect.php
+++ estufa/protect.php
@@ -19,7 +19,7 @@
 
                 echo "<div style='text-align: center; color: #325c12; font-family: \"Lucida Sans\", \"Lucida Sans Regular\", \"Lucida Grande\", \"Lucida Sans Unicode\", Geneva, Verdana, sans-serif; margin-top: 270px;'>";
                 echo "<img src='fundoLogin/erro.png' alt='Erro'><br>";
-                die("<h1>Não foi possível carregar esta página!</h1> <h3> Para acessá-la, faça o <a href=\"index.php\">login.</a> <br><br>Caso não tenha uma conta, por favor, <a href=\"cadasUsuario.php\">cadastre-se.</a></h3></div>");
+                die("<h1>Não foi possível carregar esta página!</h1> <h3> Para acessá-la, faça o <a href=\"login.php\">login.</a> <br><br>Caso não tenha uma conta, por favor, <a href=\"cadasUsuario.php\">cadastre-se.</a></h3></div>");
                                                             
             }
         ?>
